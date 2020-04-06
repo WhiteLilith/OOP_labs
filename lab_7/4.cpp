2214a9ec12a4a0289addd8ce170270a0a9e56638
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2020-04-06 20:33:55 +0300

    Update 4.cpp

--- lab_7/4.cpp
+++ lab_7/4.cpp
@@ -1,45 +1,109 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include <string>
-#include <stdlib.h>
-#include <iomanip>
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
+#include <process.h> //for exit()
+
+////////////////////////////////////////////////////////////////
+
 class Int
+
 {
+
 private:
-	int i;
+
+int i;
 
 public:
-	Int()
-	{
-		i = 0;
-	}
 
-	Int(int ii)
-	{
-		i = ii;
-	}
-	
-	Int operator + (Int i1)
-	{
-		int a;
-		a = i + i1.i;
-		return a;
-	}
+Int() : i(0) //no-arg constructor
 
-	void display()
-	{
-		cout << i;
-	}
+{ }
+
+Int(int ii) : i(ii) //1-arg constructor
+
+{ } // (int to Int)
+
+void putInt() //display Int
+
+{ cout << i; }
+
+void getInt() //read Int from kbd
+
+{ cin >> i; }
+
+operator int() //conversion operator
+
+{ return i; } // (Int to int)
+
+Int operator + (Int i2) //addition
+
+{ return checkit( long double(i)+long double(i2) ); }
+
+Int operator - (Int i2) //subtraction
+
+{ return checkit( long double(i)-long double(i2) ); }
+
+Int operator * (Int i2) //multiplication
+
+{ return checkit( long double(i)*long double(i2) ); }
+
+Int operator / (Int i2) //division
+
+{ return checkit( long double(i)/long double(i2) ); }
+
+Int checkit(long double answer) //check results
+
+{
+
+if( answer > 2147483647.0L || answer < -2147483647.0L )
+
+{ cout << "\nOverflow Error\n"; exit(1); }
+
+return Int( int(answer) );
+
+}
+
 };
 
+////////////////////////////////////////////////////////////////
+
 int main()
+
 {
-	Int a(3);
-	Int b(-10);
-	Int c = a + b;
-	c.display();
+
+Int alpha = 20;
+
+Int beta = 7;
+
+Int delta, gamma;
+
+gamma = alpha + beta; //27
+
+cout << "\ngamma="; gamma.putInt();
+
+gamma = alpha - beta; //13
+
+cout << "\ngamma="; gamma.putInt();
+
+gamma = alpha * beta; //140
+
+cout << "\ngamma="; gamma.putInt();
+
+gamma = alpha / beta; //2
+
+cout << "\ngamma="; gamma.putInt();
+
+delta = 2147483647;
+
+gamma = delta + alpha; //overflow error
+
+delta = -2147483647;
+
+gamma = delta - alpha; //overflow error
+
+cout << endl;
+
+return 0;
+
 }
