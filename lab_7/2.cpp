0ff96adec05b6cf3f8e46a639c461bdfe4f4b04d
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2020-04-06 20:22:18 +0300

    Update 2.cpp

--- lab_7/2.cpp
+++ lab_7/2.cpp
@@ -1,16 +1,10 @@
-2.
-
-// strplus.cpp
-
-// overloaded '+' operator concatenates strings
-
 #include <iostream>
+
+#include <cstring> //for strcpy(), strlen()
 
 using namespace std;
 
-#include <string.h> //for strcpy(), strcat()
-
-#include <stdlib.h> //for exit()
+#include <process.h> //for exit()
 
 ////////////////////////////////////////////////////////////////
 
@@ -20,45 +14,35 @@ class String //user-defined string type
 
 private:
 
-enum { SZ=80 }; //size of String objects
+enum { SZ = 80 }; //size of String objects
 
-char str[SZ]; //holds a string
+char str[SZ]; //holds a C-string
 
 public:
 
-String() //constructor, no args
+String() //no-arg constructor
 
 { strcpy(str, ""); }
 
-String( char s[] ) //constructor, one arg
+String( char s[] ) //1-arg constructor
 
 { strcpy(str, s); }
 
-void display() const //display the String
+void display() //display the String
 
 { cout << str; }
 
-String operator + (String ss) const //add Strings
+String operator += (String ss) //add a String to this one
 
-{
+{ //result stays in this one
 
-String temp; //make a temporary String
-
-if( strlen(str) + strlen(ss.str) < SZ )
-
-{
-
-strcpy(temp.str, str); //copy this string to temp
-
-strcat(temp.str, ss.str); //add the argument string
-
-}
-
-else
+if( strlen(str) + strlen(ss.str) >= SZ )
 
 { cout << "\nString overflow"; exit(1); }
 
-return temp; //return temp String
+strcat(str, ss.str); //add the argument string
+
+return String(str); //return temp String
 
 }
 
@@ -70,23 +54,19 @@ int main()
 
 {
 
-String s1 = "\nMerry Christmas! "; //uses constructor 2
+String s1 = "Merry Christmas! "; //uses 1-arg ctor
 
-String s2 = "Happy new year!"; //uses constructor 2
+String s2 = "Happy new year!"; //uses 1-arg ctor
 
-String s3; //uses constructor 1
+String s3; //uses no-arg ctor
 
-s1.display(); //display strings
+s3 = s1 += s2; //add s2 to s1, assign to s3
 
-s2.display();
+cout << "\ns1="; s1.display(); //display s1
 
-s3.display();
+cout << "\ns2="; s2.display(); //display s2
 
-s3 = s1 + s2; //add s2 to s1,
-
-// assign to s3
-
-s3.display(); //display s3
+cout << "\ns3="; s3.display(); //display s3
 
 cout << endl;
 
