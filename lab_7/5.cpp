28026e3861e5b2a0955cf49dc9d1e1e652ab7020
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2020-04-14 01:37:24 +0300

    Create 5.cpp

---
+++ lab_7/5.cpp
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <string>
+#include <stdlib.h>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+class Time
+
+{
+
+private:
+
+	int hrs, mins, secs;
+
+	void Check()
+	{
+		if (secs >= 60)
+		{
+			secs -= 60;
+			mins++;
+		}
+		if (mins >= 60)
+		{
+			mins -= 60;
+			hrs++;
+		}
+
+		if (secs < 0)
+		{
+			secs += 60;
+			mins--;
+		}
+		if (mins < 0)
+		{
+			mins += 60;
+			hrs--;
+		}
+	}
+
+public:
+
+	Time() : hrs(0), mins(0), secs(0)
+	{ } 
+
+	Time(int h, int m, int s)
+	{ 
+		hrs = h;
+		mins = m;
+		secs = s;
+	}
+
+	void display() 
+
+	{
+		cout << hrs << ":" << mins << ":" << secs;
+	}
+/*
+	Time& operator++()
+	{
+		++secs;
+		Check();
+		return *this;
+	}
+*/
+
+	Time& operator++()
+	{
+		Time rez(hrs, mins, ++secs);
+		rez.Check();
+		return(rez);
+	}
+	Time operator++(int)
+	{
+		Time rez(hrs, mins, secs++);
+		rez.Check();
+		return rez;
+	}
+
+	Time& operator--()
+	{
+		Time rez(hrs, mins, --secs);
+		rez.Check();
+		return rez;
+	}
+
+	Time operator--(int)
+	{
+		Time rez(hrs, mins, secs--);
+		rez.Check();
+		return rez;
+	}
+
+};
+
+int main()
+{
+	Time a(7, 2, 9);
+
+	a++.display();
+	cout << endl;
+	a.display();
+	cout << endl;
+
+	--a;
+	a--;
+	a.display();
+
+}
