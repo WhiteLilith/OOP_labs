435baa6f383a88c5cfa5c80b2f228c66fe6a56f9
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2020-05-16 16:10:57 +0300

    Update 7.cpp

--- lab_9/7.cpp
+++ lab_9/7.cpp
@@ -1,117 +1,64 @@
 #include <iostream>
+
 #include <string> //for string class
-#include <stdio.h>
-#include <iomanip>
 
 using namespace std;
 
 ////////////////////////////////////////////////////////////////
 
-class Person //class of persons
+class person //class of persons
 {
 protected:
 	string name; //person's name
 	float salary;
 
 public:
-	void setData() //set data
+	void setData() //set the name
 	{
-		cout << endl << "Enter name: ";
+		cout << endl << "Enter name: "; 
 		cin >> name;
-
 		cout << "Enter salary: ";
 		cin >> salary;
 	}
 
-	void printData() //display data
+	void printData() //display the name
+
 	{
-		cout << endl << "Name: " << name << endl;
-		cout << "Salary: " << salary << endl;
+		cout << endl << name << endl;
+		cout << salary;
 	}
 
-	string GetName() //return the name
+	string getName() //return the name
 	{
 		return name;
 	}
 
-	float GetSalary() //return the salary
+	float getSalary()
 	{
 		return salary;
 	}
 
 };
 
-void quickSort(Person *personArray[], int beg, int end)
-{
-	if (beg < end)
-	{
-		int l = beg;
-		int r = end;
-		float baza = personArray[(l + r) / 2].GetSalary();
-		Person bus;
-
-		do
-		{
-			while (personArray[l].GetSalary() < baza)
-			{
-				l++;
-			}
-
-			while (personArray[r].GetSalary() > baza)
-			{
-				r--;
-			}
-
-			if (l <= r)
-			{
-				bus = personArray[l];
-				personArray[l] = personArray[r];
-				personArray[r] = bus;
-				l++;
-				r--;
-			}
-
-		} while (l <= r);
-
-		quickSort(personArray, beg, r);
-		quickSort(personArray, l, end);
-
-	}
-}
-
-
-void order(person** pp1, person** pp2) //orders two pointers
-
-{ //if 1st larger than 2nd,
-
-	if ((*pp1)->getName() > (*pp2)->getName())
-
-	{
-
-		person* tempptr = *pp1; //swap the pointers
-
-		*pp1 = *pp2;
-
-		*pp2 = tempptr;
-
-	}
-
-}
-
 ////////////////////////////////////////////////////////////////
 
 int main()
 
 {
-	Person* persPtr[100]; //array of pointers to persons
+
+	void bsort(person**, int); //prototype
+
+	person* persPtr[100]; //array of pointers to persons
+
 	int n = 0; //number of persons in array
+
 	char choice; //input char
 
 	do { //put persons in array
 
-		persPtr[n] = new Person; //make new object
+		persPtr[n] = new person; //make new object
 
-		persPtr[n]->setData(); //set person's data
+		persPtr[n]->setData(); //set person's name
 
 		n++; //count new person
 
@@ -119,7 +66,9 @@ int main()
 
 		cin >> choice; // person?
 
-	} while (choice == 'y'); //quit on 'n'
+	}
+
+	while (choice == 'y'); //quit on 'n'
 
 	cout << "\nUnsorted list:";
 
@@ -129,7 +78,7 @@ int main()
 		persPtr[j]->printData();
 	}
 
-	quickSort(persPtr, 0, n);
+	bsort(persPtr, n); //sort pointers
 
 	cout << "\nSorted list:";
 
@@ -143,134 +92,42 @@ int main()
 
 	return 0;
 
+} //end main()
+
+//--------------------------------------------------------------
+
+void bsort(person** pp, int n) //sort pointers to persons
+
+{
+
+	void order(person**, person**); //prototype
+
+	int j, k; //indexes to array
+
+	for (j = 0; j < n - 1; j++) //outer loop
+
+		for (k = j + 1; k < n; k++) //inner loop starts at outer
+
+			order(pp + j, pp + k); //order the pointer contents
+
 }
 
-//#include <iostream>
-//
-//#include <string> //for string class
-//
-//using namespace std;
-//
-//////////////////////////////////////////////////////////////////
-//
-//class person //class of persons
-//
-//{
-//
-//protected:
-//
-//	string name; //person's name
-//
-//public:
-//
-//	void setName() //set the name
-//
-//	{
-//		cout << "Enter name: "; cin >> name;
-//	}
-//
-//	void printName() //display the name
-//
-//	{
-//		cout << endl << name;
-//	}
-//
-//	string getName() //return the name
-//
-//	{
-//		return name;
-//	}
-//
-//};
-//
-//////////////////////////////////////////////////////////////////
-//
-//int main()
-//
-//{
-//
-//	void bsort(person**, int); //prototype
-//
-//	person* persPtr[100]; //array of pointers to persons
-//
-//	int n = 0; //number of persons in array
-//
-//	char choice; //input char
-//
-//	do { //put persons in array
-//
-//		persPtr[n] = new person; //make new object
-//
-//		persPtr[n]->setName(); //set person's name
-//
-//		n++; //count new person
-//
-//		cout << "Enter another (y/n)? "; //enter another
-//
-//		cin >> choice; // person?
-//
-//	}
-//
-//	while (choice == 'y'); //quit on 'n'
-//
-//	cout << "\nUnsorted list:";
-//
-//	for (int j = 0; j < n; j++) //print unsorted list
-//
-//	{
-//		persPtr[j]->printName();
-//	}
-//
-//	bsort(persPtr, n); //sort pointers
-//
-//	cout << "\nSorted list:";
-//
-//	for (j = 0; j < n; j++) //print sorted list
-//
-//	{
-//		persPtr[j]->printName();
-//	}
-//
-//	cout << endl;
-//
-//	return 0;
-//
-//} //end main()
-//
-////--------------------------------------------------------------
-//
-//void bsort(person** pp, int n) //sort pointers to persons
-//
-//{
-//
-//	void order(person**, person**); //prototype
-//
-//	int j, k; //indexes to array
-//
-//	for (j = 0; j < n - 1; j++) //outer loop
-//
-//		for (k = j + 1; k < n; k++) //inner loop starts at outer
-//
-//			order(pp + j, pp + k); //order the pointer contents
-//
-//}
-//
-////--------------------------------------------------------------
-//
-//void order(person** pp1, person** pp2) //orders two pointers
-//
-//{ //if 1st larger than 2nd,
-//
-//	if ((*pp1)->getName() > (*pp2)->getName())
-//
-//	{
-//
-//		person* tempptr = *pp1; //swap the pointers
-//
-//		*pp1 = *pp2;
-//
-//		*pp2 = tempptr;
-//
-//	}
-//
-//}
+//--------------------------------------------------------------
+
+void order(person** pp1, person** pp2) //orders two pointers
+
+{ //if 1st larger than 2nd,
+
+	if ((*pp1)->getSalary() > (*pp2)->getSalary())
+
+	{
+
+		person* tempptr = *pp1; //swap the pointers
+
+		*pp1 = *pp2;
+
+		*pp2 = tempptr;
+
+	}
+
+}
